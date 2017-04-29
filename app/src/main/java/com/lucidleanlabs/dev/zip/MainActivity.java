f201a2f9af3010b0e5852657493d79dd96a661ba
LUCIDLEANLABS\ramky <ramky.lll@outlook.com>
2017-04-29 17:28:25 +0530

    Initial commit

---
+++ app/src/main/java/com/lucidleanlabs/dev/zip/MainActivity.java
@@ -0,0 +1,30 @@
+package com.lucidleanlabs.dev.zip;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    String Location = Environment.getExternalStorageDirectory() + "/L_CATALOGUE/Sofa_3_Seater_Black_Grey";
+    String StorezipFileLocation = Environment.getExternalStorageDirectory() + "/L_CATALOGUE/Sofa_3_Seater_Black_Grey.zip";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.text);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                UnzipUtil unzipUtil = new UnzipUtil(StorezipFileLocation, Location);
+                unzipUtil.unzip();
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/lucidleanlabs/dev/zip/UnzipUtil.java
@@ -0,0 +1,74 @@
+package com.lucidleanlabs.dev.zip;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+
+public class UnzipUtil
+{
+    private String zipFile;
+    private String location;
+
+    public UnzipUtil(String zipFile, String location)
+    {
+        this.location = Environment.getExternalStorageDirectory() + "/L_CATALOGUE/Sofa_3_Seater_Black_Grey";
+        this.zipFile = Environment.getExternalStorageDirectory() + "/L_CATALOGUE/Sofa_3_Seater_Black_Grey.zip";;
+
+        dirChecker("");
+    }
+
+    public void unzip() {
+        try {
+            FileInputStream fin = new FileInputStream(zipFile);
+            ZipInputStream zin = new ZipInputStream(fin);
+            ZipEntry ze;
+            while ((ze = zin.getNextEntry()) != null)
+            {
+                Log.v("Decompress", "Unzipping " + ze.getName());
+
+                if(ze.isDirectory())
+                {
+                    dirChecker(ze.getName());
+                }
+                else
+                {
+                    FileOutputStream fout = new FileOutputStream(location + ze.getName());
+
+                    for (int i = zin.read(); i != -1; i = zin.read()) {
+                        fout.write(i);
+                    }
+
+//                    byte[] buffer = new byte[8192];
+//                    int len;
+//                    while ((len = zin.read(buffer)) != -1)
+//                    {
+//                        fout.write(buffer, 0, len);
+//                    }
+                    fout.close();
+                    zin.closeEntry();
+
+                }
+
+            } zin.close();
+        }catch(Exception e)
+        {
+            Log.e("Decompress", "unzip", e);
+        }
+
+    }
+
+    private void dirChecker(String dir)
+    {
+        File f = new File(location + dir);
+        if(!f.isDirectory())
+        {
+            f.mkdirs();
+        }
+    }
+}
